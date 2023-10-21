dd069fe11ac7079213ddafff9ab5e84adafdf4f0
Salgado2004 <leosalgado2004@gmail.com>
2023-10-20 23:18:14 -0300

    contador de tempo e de voltas

--- js/game.js
+++ js/game.js
@@ -7,10 +7,18 @@ const canvasHeight = 576;
 scene.width = canvasWidth;
 scene.height = canvasHeight;
 
-let prevTime = 0;
+const time = {
+    minuto: 0,
+    segundo: 0
+}
+
+let lastLap = 0;
+
+let distance = 0;
 
 const playerPlace = document.querySelector(".player-place");
 const lapCount = document.querySelector(".lap-count");
+const timeCount = document.querySelector(".time");
 
 
 function animate(){
@@ -21,24 +29,34 @@ function animate(){
 
     background.update();
     road.forEach(roadSegment => roadSegment.update());
+    competitors.forEach(bot => bot.update());
     player.update();
-    competitor.update();
     words.forEach(word => word.update());
 
+    timeCount.textContent = time.minuto.toString().padStart(2, "0")+":"+time.segundo.toString().padStart(2, "0");
     playerPlace.textContent = player.place+"º Posição";
     lapCount.textContent = "Volta: "+player.lap;
 
-    // FPS
-    let delta = (performance.now() - prevTime)/1000;
-    let fps = 1 / delta;
-
-    prevTime = performance.now();
-    //console.log(`FPS: ${fps}`);
 }
 
 let dificuldade = "facil";
 
 function play(){
+    if (player.width == 0){
+        window.location.reload();
+    }
+    competitors.forEach(bot => {
+        if (bot.width == 0){
+            window.location.reload();
+        }
+    });
+    seconds = setInterval(() => {
+        time.segundo++;
+        if(time.segundo == 60){
+            time.segundo = 0;
+            time.minuto++;
+        }
+    }, 1000);
     fetch("assets/words.json")
         .then(response => response.json())
         .then(json => {
@@ -49,9 +67,21 @@ function play(){
     animate();
 }
 
+function countLap(){
+    if (lastLap > ((time.segundo+time.minuto*60)-2)){
+        return;
+    } else{
+        lastLap = time.segundo+time.minuto*60;
+        player.lap++;
+        competitors.forEach(bot => bot.updateSpeed());
+    }
+}
+
 function pause(){
     window.cancelAnimationFrame(animate);
     player.speed = 0;
+    competitors.forEach(bot => bot.speed = 0);
+    clearInterval(wordcreator);
 }
 
 play();
