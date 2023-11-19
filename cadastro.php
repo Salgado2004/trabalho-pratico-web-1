92adbca4e336ae9708214b985ea58061ef24cb0d
AlissonGabrielSantos <alisson.gab.santos@gmail.com>
2023-11-19 13:55:05 -0300

    Fazendo os checks

--- cadastro.php
+++ cadastro.php
@@ -15,59 +15,42 @@ function verifica_campo($texto){
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
     if(empty($_POST["nome-cad"])){
-      $erro_nome = "Nome é obrigatório.";
-      $erro = true;
+      $erro = 6;
+    }else if(empty($_POST["email-cad"])){
+      $erro = 1;
+    }
+    else if(!filter_var($_POST["email-cad"], FILTER_VALIDATE_EMAIL)){
+      $erro = 2;
+    } 
+    else if(empty($_POST["senha-cad"])){
+      $erro = 3;
+    }
+    else if(strlen($_POST["senha-cad"]) < 8){
+      $erro = 4;
+    }
+    else if(!preg_match('@[A-Z]@',$_POST["senha-cad"])){
+      $erro = 5;
+    }
+    else if(empty($_POST["senha-confirm"])){
+      $erro = 7;
+    }
+    else if($_POST["senha"] != $_POST["senha-conf"]){
+      $erro = 8;
     }
     else{
       $nome = verifica_campo($_POST["nome-cad"]);
-    }
-  
-    if(empty($_POST["email-cad"])){
-      $erro_email = "Email é obrigatório.";
-      $erro = true;
-    }
-    else if(!filter_var($_POST["email-cad"], FILTER_VALIDATE_EMAIL)){
-      $erro_email = "Email inválido.";
-      $erro = true;
-    } 
-    else{
       $email = verifica_campo($_POST["email-cad"]);
-    }
-  
-    if(empty($_POST["senha-cad"])){
-      $erro_senha = "Senha é obrigatória.";
-      $erro = true;
-    }
-    else if(strlen($_POST["senha-cad"]) < 8){
-      $erro_senha = "Senha deve ter pelo menos 8 caracteres.";
-      $erro = true;
-    }
-    else if(!preg_match('@[A-Z]@',$_POST["senha-cad"])){
-      $erro_senha = "Senha deve ter pelo menos uma letra maiúscula.";
-      $erro = true;
-    }
-    else{
       $senha = verifica_campo($_POST["senha-cad"]);
-    }
-  
-    if(empty($_POST["senha-confirm"])){
-      $erro_senha_conf = "Confirmação de senha é obrigatória.";
-      $erro = true;
-    }
-    else if($_POST["senha"] != $_POST["senha-conf"]){
-      $erro_senha_conf = "Confirmação de senha deve ser igual à senha.";
-      $erro = true;
-    }
-    else{
       $senha_conf = verifica_campo($_POST["senha-conf"]);
     }
+    
 
     if (!$erro) {  
         session_start();
         $_SESSION['nome_usuario'] = "User";
         header("Location: user/edit_user.php");}
     else {
-        header("Location: signup.php");
+        header("Location: signup.php/?E=" . $erro);
     }
   }
 
--- login.php
+++ login.php
@@ -1,44 +1,30 @@
 <?php
-function verifica_campo($texto){
-    $texto = trim($texto);
-    $texto = stripslashes($texto);
-    $texto = htmlspecialchars($texto);
-    return $texto;
-  }
-  
+  function verifica_campo($texto){
+      $texto = trim($texto);
+      $texto = stripslashes($texto);
+      $texto = htmlspecialchars($texto);
+      return $texto;
+    }
+
   $nome = "";
   $email = "";
   $senha = "";
-  $erro = false;
+  $erro = 0;
 
   
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
     if(empty($_POST["email-log"])){
-      $erro_email = "Email é obrigatório.";
-      $erro = true;
+      $erro = 1;
     }
     else if(!filter_var($_POST["email-log"], FILTER_VALIDATE_EMAIL)){
-      $erro_email = "Email inválido.";
-      $erro = true;
-    } 
+      $erro = 2;
+    }
+    else if(empty($_POST["senha-log"])){
+      $erro = 3;
+    }
     else{
       $email = verifica_campo($_POST["email-log"]);
-    }
-  
-    if(empty($_POST["senha-log"])){
-      $erro_senha = "Senha é obrigatória.";
-      $erro = true;
-    }
-    else if(strlen($_POST["senha-log"]) < 8){
-      $erro_senha = "Senha deve ter pelo menos 8 caracteres.";
-      $erro = true;
-    }
-    else if(!preg_match('@[A-Z]@',$_POST["senha-log"])){
-      $erro_senha = "Senha deve ter pelo menos uma letra maiúscula.";
-      $erro = true;
-    }
-    else{
       $senha = verifica_campo($_POST["senha-log"]);
     }
 
@@ -47,7 +33,7 @@ function verifica_campo($texto){
         $_SESSION['nome_usuario'] = "User";
         header("Location: user/edit_user.php");}
     else {
-        header("Location: signup.php");
+        header("Location: signup.php/?e=" . $erro);
     }
 }
 
