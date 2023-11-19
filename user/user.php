566b9ea74c476672fdf629db24c2cbd807c8ccbb
Salgado2004 <leosalgado2004@gmail.com>
2023-11-19 13:27:04 -0300

    create user profile pages

---
+++ user/user.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>World Runner | Profile</title>
+    <link rel="stylesheet" href="../style/fonts.css">
+    <link rel="stylesheet" href="../style/user.css">
+</head>
+<body>
+    <header>
+        <a id="voltar" href="../home/home.php">Voltar a Home</a>
+        <div class="sair">
+            <a href="../logout.php">Sair</a>
+        </div>
+    </header>
+    <main>
+        <iframe id="user-menu" name="user-menu" src="profile.php" frameborder="0"></iframe>
+    </main>
+    <footer>
+        &copy; TADS UFPR 2023;
+    </footer>
+</body>
+</html>
