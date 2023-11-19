d54a7082846af9cf64eab052ddd0f03fa631b094
Salgado2004 <leosalgado2004@gmail.com>
2023-11-19 15:19:47 -0300

    update database creation

---
+++ criar_bd/tables.sql
@@ -0,0 +1,36 @@
+    create table usuario (
+        id int auto_increment primary key,
+        nome varchar(20) not null unique,
+        email varchar(40) not null unique,
+        senha varchar(256) not null,
+        imagem int null,
+        carro int default 0,
+        fk_liga int null
+    );
+
+    create table liga (
+        id int auto_increment primary key,
+        nome varchar(20) not null unique,
+        senha varchar(256),
+        private boolean not null,
+        imagem int null,
+        fk_criador int not null
+    );
+
+    create table pontuacao (
+        id int auto_increment primary key,
+        modo_jogo boolean not null,
+        tempo time,
+        pountuacao int not null,
+        data_reg timestamp,
+        fk_usuario int not null
+    );
+
+    alter table usuario add constraint fk_liga_usuario
+    foreign key (fk_liga) references liga(id);
+
+    alter table liga add constraint fk_usuario_1
+    foreign key (fk_criador) references usuario(id);
+
+    alter table pontuacao add constraint fk_usuario_2
+    foreign key (fk_usuario) references usuario(id);
