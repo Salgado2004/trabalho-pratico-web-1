4a88d55e6a6a5588886b40b9aacd3ed0e1370966
Salgado2004 <leosalgado2004@gmail.com>
2023-11-26 12:47:48 -0300

    lista ranking geral e por liga

--- home/ranking.php
+++ home/ranking.php
@@ -18,6 +18,33 @@
         echo "<script>window.location.href='error_liga.php';</script>";
     }
 
+    require("../database/credentials.php");
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+    }
+
+    $sql = "use $dbname";
+    if (!mysqli_query($conn, $sql)) {
+        echo "Error connecting to database: " . mysqli_error($conn) . "<br>";
+    }
+
+    if ($_GET['scope'] == "geral"){
+        $sql = "SELECT nome, sum(pontuacao) as pontuacao FROM pontuacao INNER JOIN usuario ON pontuacao.fk_usuario = usuario.id GROUP BY usuario.id ORDER BY pontuacao DESC;";
+    } else{
+        $sql = "SELECT nome, sum(pontuacao) as pontuacao FROM pontuacao INNER JOIN usuario ON pontuacao.fk_usuario = usuario.id WHERE usuario.fk_liga = " . $_SESSION['liga_usuario'] . " GROUP BY usuario.id ORDER BY pontuacao DESC";
+    }
+
+    $ranking = array();
+    if(mysqli_query($conn, $sql)){
+        $result = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_assoc($result)){
+            $ranking[] = $row;
+        }
+    } else{
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
     $nomeUsuario = $_SESSION['nome_usuario'];
     $scope = $_GET['scope'];
 ?>
@@ -30,6 +57,19 @@
     <title>World Runner | Ranking</title>
 </head>
 <body>
-    <h1>Ranking <?php echo $scope ?></h1>
+    <?php ?>
+    <table>
+        <tr>
+            <th>Posição</th>
+            <th>Nome</th>
+            <th>Pontuação</th>
+        </tr>
+        <?php $posicao = 1; foreach($ranking as $usuario) : ?>
+            <tr>
+                <td><?php echo $posicao; ?>º</td>
+                <td><?php echo $usuario['nome']; ?></td>
+                <td><?php echo $usuario['pontuacao']; ?></td>
+            </tr>
+        <?php $posicao++; endforeach; ?>
 </body>
 </html>
