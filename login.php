d670c06a0c3c663fb0aecc75196fb121626b3694
Salgado2004 <leosalgado2004@gmail.com>
2023-11-26 12:46:22 -0300

    implementa login com o banco de dados

--- login.php
+++ login.php
@@ -29,14 +29,53 @@
     }
 
     if (!$erro) {  
-        session_start();
-        $_SESSION['nome_usuario'] = "User";
-        $_SESSION['existe_liga'] = false;
-        $_SESSION['email_usuario'] = $email;
-        $_SESSION['liga_usuario'] = "Sem liga";
-        header("Location: home/home.php");}
-    else {
-        header("Location: signup.php/?e=" . $erro);
+      require("database/credentials.php");
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+      if (!$conn) {
+        die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+      }
+
+      $sql = "use $dbname";
+      if (!mysqli_query($conn, $sql)) {
+        echo "Error connecting to database: " . mysqli_error($conn) . "<br>";
+      }
+
+      $sql = "SELECT * FROM usuario WHERE email = '$email'";
+      if (mysqli_query($conn, $sql)) {
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+          $row = mysqli_fetch_assoc($result);
+          if (md5($senha)== $row['senha']) {
+            $nome = $row['nome'];
+            $liga = $row['fk_liga'];
+            $existe_liga = false;
+            if ($liga != NULL) {
+              $existe_liga = true;
+            }
+            session_start();
+            $_SESSION['id_usuario'] = $row['id'];
+            $_SESSION['nome_usuario'] = $nome;
+            $_SESSION['existe_liga'] = $existe_liga;
+            $_SESSION['email_usuario'] = $email;
+            $_SESSION['liga_usuario'] = $liga;
+            $_SESSION['estiloCarro'] = $row['carro'];
+            header("Location: home/home.php");
+          }
+          else {
+            $erro = 4;
+            echo "Senha incorreta";
+          }
+        }
+        else {
+          $erro = 5;
+          echo "Email não cadastrado";
+        }
+      }
+      else {
+        echo "Error connecting to database: " . mysqli_error($conn) . "<br>";
+      }
+    } else {
+      header("Location: signup.php/?e=" . $erro);
     }
 }
 
